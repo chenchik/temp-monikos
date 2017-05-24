2a07ee2560e8bfd5c7183372120eb480e1381bb3
WeijianL1 <liweijian1996@gmail.com>
2017-05-23 20:00:22 -0400

    New landing page and popup window

--- mvc/public/js/account/accountCtrl.js
+++ mvc/public/js/account/accountCtrl.js
@@ -1,83 +1,142 @@
-//Created by Danila Chenchik Monikos LLC
+//Created by Danila Chenchik, Joseph Son Monikos LLC
 
 var app = angular.module('myApp', []);
-app.controller('accountCtrl', function($scope, $http) {
 
-    var url2 = "/db/get_schools.php";
-    $http.get(url2).then(function (response) {
+app.controller('accountCtrl', function ($scope, $http) {
+    var url7 = "/db/get_programs.php";
+    $http.get(url7).then(function (response) {
         console.log('ww');
         console.log(response);
-        $scope.schoolnames = response.data.records;
-        console.log($scope.schoolnames);
-    });
+        $scope.programs = response.data.records;
+        for (var i = 0; i < 6; i++) {
+            switch ($scope.programs[i].program) {
+                case "do":
+                    $scope.programs[i].program = "D.O";
+                    break;
+                case "nursing":
+                    $scope.programs[i].program = "Nursing";
+                    break;
+                case "pharmacy":
+                    $scope.programs[i].program = "Pharmacy";
+                    break;
+                case "med":
+                    $scope.programs[i].program = "Medicine";
+                    break;
+                case "pa":
+                    $scope.programs[i].program = "PA";
+                    break;
+                case "dental":
+                    $scope.programs[i].program = "Dental";
+                    break;
+            }
+        }
+        console.log($scope.programs);
+    })
 
-    $scope.closeError = function(){
+    $scope.closeError = function () {
 
     }
 
-    function showError(str){
+    $scope.findSchool = function () {
+        var pro = $('#program').val();
+        var url;
+        if (pro == "Nursing") {
+            var url = "/db/get_nursing_schools.php";
+        } else if (pro == "Pharmacy") {
+            var url = "/db/get_pharm_schools.php";
+        } else if (pro == "Medicine") {
+            var url = "/db/get_med_schools.php";
+        } else if (pro == "PA") {
+            var url = "/db/get_pa_schools.php";
+        } else if (pro == "D.O") {
+            var url = "/db/get_do_schools.php";
+        } else if (pro = "Dental") {
+            var url = "/db/get_dental_schools.php";
+        }
+        $http.get(url).then(function (response) {
+            console.log('ww');
+            console.log(response);
+            $scope.schoolnames = response.data.records;
+            console.log($scope.schoolnames);
+        });
+    }
+
+
+    function showError(str) {
         $('#errorMessage').slideDown('fast');
         $('.errorText').html(str);
     }
 
+
     function validateEmail(email) {
-      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(email);
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
     }
 
     function checkEmail(email) {
 
-      if (!validateEmail(email)) {
-        showError("The email you enetered is not valid");
-        return false;
-      }
-      return true;
-    }
-
-
-
-    function lengthChecker(str, field){
-        if(str.length > 2) {
-            return true;
-        }else{
-            showError("Please enter a " + field + " greater than 2 characters long.");
-            return false;
-        }
-    }
-
-    function checkAplhaNumeric(str, field){
-        var Exp = /^([0-9]|[a-z])+([0-9a-z]+)$/i;
-
-        if(!str.match(Exp)){
-            showError("Please only use numbers or letters in the " + field + " field.");
-            return false;
-        }else{
-            return true;
-        }
-    }
-
-    function fieldChecker( str, field){
-
-        if(!lengthChecker(str, field)){
-            return false;
-        }else if(!checkAplhaNumeric(str, field)){
-            return false;
-        }else if($('#school').val() == null){
-            showError("Please select a school.");
+        if (!validateEmail(email)) {
+            showError("The email you enetered is not valid");
             return false;
         }
         return true;
     }
 
-    $scope.checkUsername = function(){
 
-        var un = document.getElementById('un').value;
-        if(!fieldChecker(un, 'username')){
+
+    function lengthChecker(str, field) {
+        if (str.length > 2) {
+            return true;
+        } else {
+            showError("Please enter a " + field + " greater than 2 characters long.");
             return false;
         }
-        var email = document.getElementById('email').value;
-        var pw = document.getElementById('pw').value;
-        if(!fieldChecker(pw, 'password')){
+    }
+
+    function passwordSameChecker(str, str2) {
+        if(str===str2){return true;}
+        else{
+            showError("Please enter the same password.")
+            return false;
+        }
+    }
+
+    function checkAplhaNumeric(str, field) {
+        var Exp = /^([0-9]|[a-z])+([0-9a-z]+)$/i;
+
+        if (!str.match(Exp)) {
+            showError("Please only use numbers or letters in the " + field + " field.");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function fieldChecker(str, field) {
+
+        if (!lengthChecker(str, field)) {
+            return false;
+        } else if (!checkAplhaNumeric(str, field)) {
+            return false;
+        } else if ($('#program').val() == null) {
+            showError("Please select a program.");
+            return false;
+        }
+        return true;
+    }
+
+    $scope.checkUsername = function () {
+        var un = document.getElementById('un_reg').value;
+        if (!fieldChecker(un, 'username')) {
+            return false;
+        }
+        var email = document.getElementById('email_reg').value;
+        var pw = document.getElementById('pw_reg').value;
+        var pw2 = document.getElementById('pw2_reg').value;
+        if (!fieldChecker(pw, 'password')) {
+            return false;
+        }
+        if (!passwordSameChecker(pw,pw2)){
             return false;
         }
         var url = "/db/check_username.php";
@@ -88,34 +147,31 @@ app.controller('accountCtrl', function($scope, $http) {
         });
 
         var config = {
-            headers : {
+            headers: {
                 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
             }
         };
 
         $http.post(url, data, config)
             .then(function (response) {
-            console.log(response);
+                console.log(response);
 
-            $scope.response = response;
-            if(response.data.records[0].user){
-                showError("That username is already taken.");
-            }else if(!response.data.records[0].email){
-                showError("Please enter a valid email.");
-            }else{
-                $scope.createAccount();
-            }
-        });
-
-
-
+                $scope.response = response;
+                if (response.data.records[0].user) {
+                    showError("That username is already taken.");
+                } else if (!response.data.records[0].email) {
+                    showError("Please enter a valid email.");
+                } else {
+                   $scope.createAccount();
+                }
+            });
 
     }
 
-    $scope.createAccount = function(){
-        var un = document.getElementById('un').value;
-        var email = document.getElementById('email').value;
-        var pw = document.getElementById('pw').value;
+    $scope.createAccount = function () {
+        var un = document.getElementById('un_reg').value;
+        var email = document.getElementById('email_reg').value;
+        var pw = document.getElementById('pw_reg').value;
         var schoolid = document.getElementById('school').value;
         var tempname = "a" + schoolid;
         var schoolname = document.getElementById(tempname).innerHTML;
@@ -131,7 +187,7 @@ app.controller('accountCtrl', function($scope, $http) {
             schoolname: schoolname
         });
         var config = {
-            headers : {
+            headers: {
                 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
             }
         };
@@ -139,22 +195,25 @@ app.controller('accountCtrl', function($scope, $http) {
 
         $http.post(url, data, config)
             .then(function (response) {
-            console.log(response);
-            $scope.response = response;
-            if(response.data[0].response == 200){
-                window.location = window.location.origin + "/mvc/public/account/login";
-            }else{
-                alert("error in creating account");
-            }
-        });
+                console.log(response);
+                $scope.response = response;
+                if (response.data[0].response == 200) {
+                   $scope.login();
+                } else {
+                    alert("error in creating account");
+                }
+            });
+        
     }
-    $scope.login = function(){
+    $scope.login = function () {
         window.location = window.location.origin + "/mvc/public/account/login";
     }
 });
 
-$( document ).ready(function() {
-    $('#errorBtn').on('click', function(){
+angular.bootstrap(document.getElementById('register_app'),['myApp']);
+
+$(document).ready(function () {
+    $('#errorBtn').on('click', function () {
         $('#errorMessage').slideUp('fast');
     });
 });
