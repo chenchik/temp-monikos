7b003f8fa07f99e5f64caef7bb04ca0e46362cf7
bereshine <shuow@live.unc.edu>
2017-06-20 20:41:28 -0400

    add 'friend object', avoid duplicate friend.

--- db/add_friend_test.php
+++ db/add_friend_test.php
@@ -1,22 +1,22 @@
 <?php 
 
-$dir = '../../vendor/';
+// $dir = '../../vendor/';
 
 
-require_once $dir . 'autoload.php';
+// require_once $dir . 'autoload.php';
 
 
-echo $dir . 'autoload.php';
+// echo $dir . 'autoload.php';
 
-if (is_readable($dir . 'autoload.php')) {
-    echo 'The file is readable';
-} else {
-    echo 'The file is not readable';
-}
+// if (is_readable($dir . 'autoload.php')) {
+//     echo 'The file is readable';
+// } else {
+//     echo 'The file is not readable';
+// }
 
-echo extension_loaded("mongodb") ? "yesloaded\n" : "not loaded\n";
- $client = new MongoDB\Client("mongodb://root:kc3irtapdnayeli29r@104.236.241.100:27017");
-
+// echo extension_loaded("mongodb") ? "yesloaded\n" : "not loaded\n";
+//  $client = new MongoDB\Client("mongodb://root:kc3irtapdnayeli29r@104.236.241.100:27017");
+require_once 'db_init.php';
  $collection = $client->monikos->Users;
 
   // $result = $collection->insertOne( [ 'yo' => 'ganxie', 'shangdi' => 'sos' ] );
@@ -25,11 +25,26 @@ echo extension_loaded("mongodb") ? "yesloaded\n" : "not loaded\n";
 
 // $collection = $client->demo->beers;
 
- $result = $collection->find( [ 'username' => 'sifron' ] );
+ $result = $collection->find( [ 'username' => 'greer' ] );
 
  foreach ($result as $entry) {
-     echo $entry['_id'], ': ', "\n<br><br>";
-     $collection->updateOne(array("_id"=>$entry['_id']),array('$addToSet' => array("friend_list"=>"kevinjeffay")));
+  $flag = True;
+   foreach ($entry['friend_list'] as $friend){
+     echo $friend['username'];
+     if ($friend['username']==$entry['username']){
+      $flag = False;
+     }
+// $dup = $friend->findOne(['username'=>$entry['username']]).count(); 
+    // if ($friend['username'] )
+   }
+
+ if ($flag){
+         echo $entry['username'], ': ', "\n<br><br>";
+     $collection->updateOne(array("_id"=>$entry['_id']),array('$addToSet' => array("friend_list"=>$entry)));
+      }
+  // if ($entry->find('friend_list'=>))
+     // echo $entry['username'], ': ', "\n<br><br>";
+     // $collection->updateOne(array("_id"=>$entry['_id']),array('$addToSet' => array("friend_list"=>$entry)));
     
 
  }
