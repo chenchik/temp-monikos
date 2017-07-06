e422acd574e50d77315638b38fa7dc24a0080d85
WeijianL1 <liweijian1996@gmail.com>
2017-07-06 14:59:33 -0400

    send email to user 2 fixed

--- db/send_email_user2_old.php
+++ db/send_email_user2_old.php
@@ -7,15 +7,23 @@ header("Content-Type: application/json; charset=UTF-8");
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-require_once "db_init.php";
+require_once 'db_creds.php';
 
-$collection= $client->monikos->Users;
-$result=$collection->findOne(["username"=>$_POST['user2']]);
+$result = $conn->query("SELECT email FROM Users WHERE username LIKE '".$_POST['user2'] ."'");
+
 
 $user2email = "";
+$count = 0;
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+	$count++;
+	$user2email = $rs['email'];	
+}
 
-if($result!=null){	
-	$to      =  $result["email"];
+if($count < 1){
+	echo '{"error":"no email"}'; 
+}else{
+	
+	$to      =  $user2email;
 	$subject = 'Monikos Challenge From ' . $_POST['user1'];
 	
     $message = 'Looks like ' . $_POST['user1'] . ' has challenged you to play the Monikos ' . $_POST['game'] . ' game. They have bet you ' . $_POST['bet'] . ' capsules that they can beat you. To accept, go to the url after this sentence, the game will start automatically.' . $_POST['url'];
@@ -23,11 +31,13 @@ if($result!=null){
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
     $headers .= 'From: Monikos <monikosguide@monikos.com>' . "\r\n";
+    
 	if(mail($to, $subject, $message, $headers)){
-        echo '{"emailsuccess":"'.$result["email"].'"}';
+        echo '{"emailsuccess":"'.$user2email.'"}';
     } else {
         echo '{"emailfailure":"failedtosendemail"}';
     }
-}else{echo '{"error":"no email"}'; }
+}
+$conn->close();
 
 ?>
--- db/test.php
+++ db/test.php
@@ -10,107 +10,27 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-require_once 'db_init.php';
-if(true){
-	$output="";
-	$collection= $client->monikos->Users;
-	$previous = $collection -> findOne(
-		['username'=> "mongo"]
-		);
-	// $capsule = $previous['capsules']- $_POST['bet'];
-	$update = $collection->updateOne(
-		['username'=> "mongo"],
-		['$set' => ['capsules'=>300]]
-		);
-	$count =$update->getModifiedCount();
-	var_dump($previous);
-	if($count==1){
-		// $output='[{"response":"200"}]';
-		$output .= '[{
-	    "response": 200,
-	    "user2": "'.$_POST["user2"].'",
-	    "lost": "'.$_POST["bet"].'"},';
 
-	}else{
-		echo '[{"response":"400"}]';
-	}
+require_once "db_init.php";
 
-	$previous = $collection -> findOne(
-		['username'=> "mongo2"]
-		);
-	$capsule = $previous['capsules']+ $_POST['bet'];
-	$update = $collection->updateOne(
-		['username'=> "mongo2"],
-		['$set' => ['capsules'=>$capsule]]
-		);
-	$count =$update->getModifiedCount();
-	if($count==1){
-		// $output='[{"response":"200"}]';
-		$output .= '{
-	    "response": 200,
-	    "user1": "'.$_POST["user1"].'",
-	    "won": "'.$_POST["bet"].'"}]';
-
-	}else{
-		echo '[{"response":"400"}]';
-	}
-	echo $output;	
+$collection= $client->monikos->Users;
+$result=$collection->findOne(["username"=>"mongo"]);
 
 
-
-}else if($_POST['user1score'] > $_POST['user2score']){
-	$output="";
+if($result!=null){	
+	$to      =  $result["email"];
+	$subject = 'Monikos Challenge From ';
 	
-	$collection= $client->minokos->Users;
-	$previous = $collection -> findOne(
-		['username'=> $_POST['user1']]
-		);
-	$capsule = $previous['capsules']- $_POST['bet'];
-	$update = $collection->updateOne(
-		['username'=> $_POST['user1']],
-		['$set' => ['capsules'=>$capsule]]
-		);
-	$count =$update->getModifiedCount();
-	$new = $collection->findOne(
-		['username'=> $_POST['user1']]
-		);
-	if($count==1){
-		$output='[{"response":"200"}]';
-		// $output .= '[{
-	 //    "response": 200,
-	 //    "user1": "'.$_POST["user1"].'",
-	 //    "lost": "'.$_POST["bet"].'"},';
+    // $message = 'Looks like ' . $_POST['user1'] . ' has challenged you to play the Monikos ' . $_POST['game'] . ' game. They have bet you ' . $_POST['bet'] . ' capsules that they can beat you. To accept, go to the url after this sentence, the game will start automatically.' . $_POST['url'];
+	$message="hello world";
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+    $headers .= 'From: Monikos <monikosguide@monikos.com>' . "\r\n";
+	if(mail($to, $subject, $message, $headers)){
+        echo '{"emailsuccess":"'.$result["email"].'"}';
+    } else {
+        echo '{"emailfailure":"failedtosendemail"}';
+    }
+}else{echo '{"error":"no email"}'; }
 
-	}else{
-		echo '[{"response":"400"}]';
-	}
-
-	
-	$collection= $client->minokos->Users;
-	$previous = $collection -> findOne(
-		['username'=> $_POST['user2']]
-		);
-	$capsule = $previous['capsules']+ $_POST['bet'];
-	$update = $collection->updateOne(
-		['username'=> $_POST['user2']],
-		['$set' => ['capsules'=>$capsule]]
-		);
-	$count =$update->getModifiedCount();
-	$new = $collection->findOne(
-		['username'=> $_POST['user2']]
-		);
-	if($count==1){
-		$output='[{"response":"200"}]';
-		// $output .= '{
-	 //    "response": 200,
-	 //    "user2": "'.$_POST["user2"].'",
-	 //    "won": "'.$_POST["bet"].'"}]';
-
-	}else{
-		echo '[{"response":"400"}]';
-	}
-	echo $output;	
-}else{
-	echo '[{"winner":"tie"}]';
-}
 ?>
