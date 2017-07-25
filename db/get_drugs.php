137b8eb792d042134465569808bd514aaa791452
WeijianL1 <liweijian1996@gmail.com>
2017-07-24 23:44:40 -0400

    handle dotted filed error in mongoDB

--- db/get_drugs.php
+++ db/get_drugs.php
@@ -4,36 +4,30 @@ header("Content-Type: application/json; charset=UTF-8");
 
 require_once 'db_init.php';
 $collection =$client->monikos->Drugs;
-
+function dotParser($string){
+    return str_replace("_", ".", $string);
+}
 $result=$collection->find([],['sort'=>["Generic"=>1],]);
-
-
 $outp = "";
-
-
-
 foreach ($result as $drug) {
-    
     if ($outp != "") {$outp .= ",";}
     $outp .= '{"Generic":"'  . $drug['Generic'] . '",';
-    $outp .= '"DrugId":"'   . $drug["_id"]        . '",';
-    $outp .= '"Brand":'   . json_encode($drug['Brand'])        . ',';
-    $outp .= '"Class":'   . json_encode($drug["Class"])        . ',';
-    $outp .= '"Indication":'   . json_encode($drug["Indication and Dosage"])        . ',';
-    $outp .= '"Side Effects":'   . json_encode($drug["Side Effects"])        . ',';
-    $outp .= '"Black Box Warning":'   . json_encode($drug["Black Box Warning"])        . ',';
-    $outp.='"Renal Adjustment":'.json_encode($drug["Renal Adjustment"]).',';
-    $outp.='"Hepatic Adjustment":'.json_encode($drug["Hepatic Adjustment"]).',';
-    $outp.='"Mechanism of Action":'.json_encode($drug["Mechanism of Action"]).',';
+    $outp .= '"DrugId":"'   . $drug["_id"].'",';
+    $outp .= '"Brand":'   . dotParser(json_encode($drug['Brand'])).',';
+    $outp .= '"Class":'   . dotParser(json_encode($drug["Class"])).',';
+    $outp .= '"Indication":'   . dotParser(json_encode($drug["Indication & Dosage"])).',';
+    $outp .= '"Side Effects":'   . dotParser(json_encode($drug["Side Effects"])).',';
+    $outp .= '"Black Box Warning":'   . dotParser(json_encode($drug["Black Box Warning"])).',';
+    $outp.='"Renal Adjustment":'.dotParser(json_encode($drug["Renal Adjustment"])).',';
+    $outp.='"Hepatic Adjustment":'.dotParser(json_encode($drug["Hepatic Adjustment"])).',';
+    $outp.='"Mechanism of Action":'.dotParser(json_encode($drug["Mechanism of Action"])).',';
 
-    $outp.='"Resources":'.json_encode($drug["Resources"]).',';
-    $outp.='"Generic Audio":'.json_encode($drug["Generic Audio"]).',';
-    $outp.='"Brand Audio":'.json_encode($drug["Brand Audio"]).',';
-    $outp.='"Hint":'.json_encode($drug["Hint"]).',';
-    $outp.='"Likes":'.json_encode($drug["Likes"]).',';
-    $outp.='"Dislikes":'.json_encode($drug["Dislikes"]).'}';
-    
-    
+    $outp.='"Resources":'.dotParser(json_encode($drug["Resources"])).',';
+    $outp.='"Generic Audio":'.dotParser(json_encode($drug["Generic Audio"])).',';
+    $outp.='"Brand Audio":'.dotParser(json_encode($drug["Brand Audio"])).',';
+    $outp.='"Hint":'.dotParser(json_encode($drug["Hint"])).',';
+    $outp.='"Likes":'.dotParser(json_encode($drug["Likes"])).',';
+    $outp.='"Dislikes":'.dotParser(json_encode($drug["Dislikes"])).'}';
 }
 $outp ='{"records":['.$outp.']}';
 
--- db/test.php
+++ db/test.php
@@ -1,22 +1,38 @@
-<?php
-
-/*Created by Danila Chenchik Monikos LLC*/
-
+ <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-require_once 'db_init.php'; 
-$collection= $client->monikos->Users;   
-$result=$collection->find([],['limit'=>142]);
+require_once 'db_init.php';
+$collection =$client->monikos->Drugs;
+function dotParser($string){
+    return str_replace("_", ".", $string);
+}
+$result=$collection->find(["Generic"=>"simvastatin"]);
+$outp = "";
+foreach ($result as $drug) {
+    if ($outp != "") {$outp .= ",";}
+    $outp .= '{"Generic":"'  . $drug['Generic'] . '",';
+    $outp .= '"DrugId":"'   . $drug["_id"].'",';
+    $outp .= '"Brand":'   . dotParser(json_encode($drug['Brand'])).',';
+    $outp .= '"Class":'   . dotParser(json_encode($drug["Class"])).',';
+    $outp .= '"Indication":'   . dotParser(json_encode($drug["Indication & Dosage"])).',';
+    $outp .= '"Side Effects":'   . dotParser(json_encode($drug["Side Effects"])).',';
+    $outp .= '"Black Box Warning":'   . dotParser(json_encode($drug["Black Box Warning"])).',';
+    $outp.='"Renal Adjustment":'.dotParser(json_encode($drug["Renal Adjustment"])).',';
+    $outp.='"Hepatic Adjustment":'.dotParser(json_encode($drug["Hepatic Adjustment"])).',';
+    $outp.='"Mechanism of Action":'.dotParser(json_encode($drug["Mechanism of Action"])).',';
 
+    $outp.='"Resources":'.dotParser(json_encode($drug["Resources"])).',';
+    $outp.='"Generic Audio":'.dotParser(json_encode($drug["Generic Audio"])).',';
+    $outp.='"Brand Audio":'.dotParser(json_encode($drug["Brand Audio"])).',';
+    $outp.='"Hint":'.dotParser(json_encode($drug["Hint"])).',';
+    $outp.='"Likes":'.dotParser(json_encode($drug["Likes"])).',';
+    $outp.='"Dislikes":'.dotParser(json_encode($drug["Dislikes"])).'}';
+}
+$outp ='{"records":['.$outp.']}';
 
-foreach ($result as $key) {
-	$md5=md5($key['password']);
-	 // echo $md5."; ".$key['password'].;
-	 // echo sizeof($key);
-	 $update=$collection->updateOne(
-	 	['_id'=>new MongoDB\BSON\ObjectID($key['_id'])],
-	 	['$set'=>['password'=>$md5]]);
-}//for md5 hash all old password
+// $conn->close();
+
+echo($outp);
 
 ?>
